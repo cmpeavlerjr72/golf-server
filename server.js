2001316311eb45767ace81599adb7729daaab351
Mitchell <cmpeavlerjr@yahoo.com>
2025-01-23 15:06:06 -0500

    Update port

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 // Middleware
 app.use(cors());
