b2ea63d3c25c9b9a274ab98ce515e4acf46f3321
Mitchell <cmpeavlerjr@yahoo.com>
2025-01-23 15:14:55 -0500

    more port alteration

--- server.js
+++ server.js
@@ -235,6 +235,6 @@ app.post('/update-data', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
